6a9bacac1056200a1ee33c5f5760ec38622f9118
BilalRifay <150447977+BilalRifay@users.noreply.github.com>
2024-07-08 20:00:11 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { Button } from './components/ui/button';
+import { Card } from './components/ui/card';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="m-10">
+      <Card>
+        <h2>Lorem, ipsum.
+        </h2>
+        <p>Lorem ipsum dolor sit amet.</p>
+      <Button>
+        Click
+      </Button>
+      </Card>
     </div>
   );
 }
