d80b9c413b982f4bc6efbb2026387e3fa347f88d
BilalRifay <md.bilalrifay@gmil.com>
2024-08-03 16:50:43 +0530

    app.test was changed

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import { act } from 'react'; 
 import App from './App';
 
 test('renders learn react link', () => {
-  act(() => {
-    render(<App />);
-  });
+  render(<App />);  // No need to wrap in act
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
